ba0320d3a01ab833c5c200dd8714c6b0b7451d0a
M Nouman <mnouman.developer@gmail.com>
2026-02-22 21:58:09 +0500

    add middleware

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 import cv2
 import shutil
 import os
 
 app = FastAPI()
+
+# CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://lovable.dev/projects/b3308eab-e50a-4483-b207-3036e6b4adca"],  # change this later to your frontend domain
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/process")
 async def process_image(file: UploadFile = File(...)):
